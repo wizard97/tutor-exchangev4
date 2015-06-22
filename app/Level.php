c539da826ad299cfaad4502efe716257e802a8b2
Aaron Wisner <aaronwisner@gmail.com>
2015-06-22 03:41:23 +0000

    search functionality and pagination, save form input in session

--- app/Level.php
+++ app/Level.php
@@ -16,4 +16,15 @@ class Level extends Model
     return $this->hasMany('App\TutorLevel', 'level_id', 'id');
   }
 
+  //return ids of matches
+  public function scopeFindTutors($query)
+  {
+    //get the classes and levels, and join that on the tutor_levels table
+  return $query
+  ->join('tutor_levels', 'levels.id', '=', 'tutor_levels.level_id')
+  ->join('tutors', 'tutors.user_id', '=', 'tutor_levels.user_id')
+  ->select(\DB::raw('count(*) as class_matches, tutor_levels.user_id'))
+  ->groupBy('tutor_levels.user_id')->orderBy('class_matches', 'desc');
+  }
+
 }
