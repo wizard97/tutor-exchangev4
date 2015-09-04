c613853d8a694fa55465bcd4c5dc587fa4ffa840
Matan Silver <matansilver@gmail.com>
2015-09-03 21:41:41 -0400

    another try, sorry for the bad messages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,179 +2,159 @@
 @extends('app')
 
 @section('content')
+<style>
+.vertical-align {
+  display: flex;
+  align-items: center;
+}
+</style>
 <div class="container">
-@include('templates/feedback')
-<div class="jumbotron">
-  <div class="container">
-  <div class="col-md-8">
-    <img src="/img/logo2.png" class="img-responsive" />
- <!-- <h1>Lexington Tutor Exchange</h1> -->
-    <h2>Hello, Lexington MA!</h2>
-    <p>This site was designed by Lexington students for Lexington students. We will find the best tutor for you based on pricing, subject, and tutor level. This site welcomes both student and professional tutor signups.</p>
-    <p>
-    <a class="btn btn-success btn-lg" href="/search/index" role="button"><span class="glyphicon glyphicon-search" aria-hidden="true"></span> Find a Tutor</a>
-    <a class="btn btn-primary btn-lg" href="@if(\Auth::check()) {{ route('tutoring.dashboard', ['id' => \Auth::user()->id]) }} @else {{ route('auth.login') }} @endif" role="button"><span class="glyphicon glyphicon-book" aria-hidden="true"></span> Start Tutoring</a>
-    </px>
+  @include('templates/feedback')
+  <div class="jumbotron">
+    <div class="container">
+      <img src="/img/logo2.png" class="img-responsive" />
+      <!-- <h1>Lexington Tutor Exchange</h1> -->
+      <h2>Hello!</h2>
+      <p>This site was designed by Lexington High School seniors with the vision of connecting tutors and students across towns, schools, and disciplines. We hope that through the use of our unique site, you can give yourself a more fulfilling education. Thank you!</br><em>~The Creators</em></p>
+      <center>
+        <div class="btn-group">
+          <a class="btn btn-success btn-lg" href="/search/index" role="button"><span class="glyphicon glyphicon-search" aria-hidden="true"></span> Find a Tutor</a>
+          <a class="btn btn-primary btn-lg" href="@if(\Auth::check()) {{ route('tutoring.dashboard', ['id' => \Auth::user()->id]) }} @else {{ route('auth.login') }} @endif" role="button"><span class="glyphicon glyphicon-book" aria-hidden="true"></span> Start Tutoring</a>
+        </div>
+      </center>
+
+    </div>
+
   </div>
+  <div class="row">
+    <div class="col-md-10 col-md-offset-1">
+      <!--<div class="panel panel-default">
+      <div class="panel-body">-->
 
-  <div class="col-md-4 hidden-sm hidden-xs">
-  <img  width="300" src="/img/large-logo-black.png">
-  </div>
-<!--
-<div class="col-md-4 hidden-sm hidden-xs">
-<h1><i class="fa fa-exchange fa-5x"></i></h1>
-</div>
--->
-  </div>
-</div>
+      <!-- <div class="alert alert-success" role="alert"><strong>New in V1.12:</strong> Save tutor button now works without page refresh (using ajax requests and jQuery), Crontab jobs now auto emails tutor when listing expires.</div> -->
+      <div id="carousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#carousel" data-slide-to="0" class="active"></li>
+          <li data-target="#carousel" data-slide-to="1"></li>
+          <li data-target="#carousel" data-slide-to="2"></li>
+          <li data-target="#carousel" data-slide-to="3"></li>
+        </ol>
 
-<!-- <div class="alert alert-success" role="alert"><strong>New in V1.12:</strong> Save tutor button now works without page refresh (using ajax requests and jQuery), Crontab jobs now auto emails tutor when listing expires.</div> -->
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+          <div class="item active">
+            <div class="col-md-8 col-md-offset-2">
+              <div class="panel panel-default box-a homestats">
+                <div class="panel-body">
+                  <small class="social-title" style="font-size: 42px">Members:</small>
 
-<div class="row social">
-
-    <div class="col-sm-6 col-md-6 col-lg-3">
-        <div class="panel panel-default box-a">
-            <div class="panel-body">
-              <small class="social-title">Members</small>
-              <h3 class="count">
-              <span class="integers">{{$stats->std_members + $stats->tutor_members}}
-              </span></h3>
-              <i class="fa-stat fa fa-users"></i>
+                  <h3 class="count">
+                    <span class="integers" style="font-size: 56px; margin-left: 40px">{{$stats->std_members + $stats->tutor_members}}
+                    </span>
+                  </h3>
+                  <i class="fa-stat fa fa-users"></i>
+                </div>
+              </div>
             </div>
+          </div>
+          <div class="item">
+            <div class="col-md-8 col-md-offset-2">
+              <div class="panel panel-default box-b homestats">
+                <div class="panel-body">
+                  <small class="social-title" style="font-size: 42px">Active Tutors:</small>
+
+                  <h3 class="count">
+                    <span class="integers" style="font-size: 56px; margin-left: 40px">{{ \App\Tutor::where('tutor_active', '1')->where('profile_expiration', '>=', date("Y-m-d H:i:s"))->count() }}
+                    </span>
+                  </h3>
+                  <i class="fa-stat fa fa-graduation-cap"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div class="col-md-8 col-md-offset-2">
+              <div class="panel panel-default box-c homestats">
+                <div class="panel-body">
+                  <small class="social-title" style="font-size: 42px">Tutor Searches:</small>
+                  <h3 class="count"><span class="integers" style="font-size: 56px; margin-left: 40px">{{ $stats->searches }}
+                  </span></h3>
+                  <i class="fa-stat fa fa-search"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div class="col-md-8 col-md-offset-2">
+              <div class="panel panel-default box-d homestats">
+                <div class="panel-body">
+                  <small class="social-title" style="font-size: 42px">Tutor Matches:</small>
+                  <h3 class="count">
+                    <span class="integers" style="font-size: 56px; margin-left: 40px">{{ \App\TutorContact::count() }}
+                    </span>
+                  </h3>
+                  <i class="fa-stat fa fa-envelope"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" style="background-image:none" href="#carousel" role="button" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left" style="color: black" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" style="background-image:none" href="#carousel" role="button" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right" style="color: black" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-6">
+    <div class="panel panel-warning">
+      <div class="panel-heading">
+        New Features:
+      </div>
+      <div class="panel-body">
+          <ul>
+            <li>New Bootstrap theme!</li>
+            <li>Improved search function</li>
+            <li>Revamped town/school/class/etc heirarchy</li>
+            <li>New <a href="/LextutorexchangeTermsofUse.pdf" target="_blank">Terms of Use</a> and <a href="/LextutorexchangePrivacyPolicy.pdf" target="_blank">Privacy Policy</a>.</li>
+            <li>Improved professional/standard tutor differentiation</li>
+            <li>Scheduling function in search and tutoring dashboard</li>
+            <li>Google Maps API for more accurate distance calculations in search</li>
+          </ul>
         </div>
       </div>
-
-    <div class="col-sm-6 col-md-6 col-lg-3">
-        <div class="panel panel-default box-b">
-            <div class="panel-body">
-                <small class="social-title">Active Tutors</small>
-                <h3 class="count">
-                <span class="integers">{{ \App\Tutor::where('tutor_active', '1')->where('profile_expiration', '>=', date("Y-m-d H:i:s"))->count() }}
-                </span></h3>
-                <i class="fa-stat fa fa-graduation-cap"></i>
-            </div>
-        </div>
     </div>
-
-
-
-    <div class="col-sm-6 col-md-6 col-lg-3">
-        <div class="panel panel-default box-c">
-            <div class="panel-body">
-                <small class="social-title">Tutor Searches</small>
-                <h3 class="count"><span class="integers">{{ $stats->searches }}
-                </span></h3>
-                <i class="fa-stat fa fa-search"></i>
-            </div>
+    <div class="col-md-6">
+      <div class="panel panel-warning">
+        <div class="panel-heading">
+          Upcoming Features:
         </div>
+        <div class="panel-body">
+          <ul>
+            <li>Inbuilt messaging</li>
+            <li>Online payment system</li>
+            <li>Multi-town support</li>
+            <li>Satisfaction guarranteed or your money back on tutoring sessions paid for through our upcoming online payment system.</li>
+            <li>Crowdsourcing the addition of new schools, classes, levels, instruments, etc.</li>
+          </ul>
+        </div>
+      </div>
     </div>
-
-    <div class="col-sm-6 col-md-6 col-lg-3">
-        <div class="panel panel-default box-d">
-            <div class="panel-body">
-                <small class="social-title">Tutor Matches</small>
-                <h3 class="count">
-                <span class="integers">{{ \App\TutorContact::count() }}
-                </span></h3>
-                <i class="fa-stat fa fa-envelope"></i>
-            </div>
-        </div>
-    </div>
-
-</div>
-
-
-<div class="row">
-<div class="col-md-6">
-<div class="panel panel-success">
-  <div class="panel-heading">
-    <h3 class="panel-title"><i class="fa fa-plus"></i> Latest Features</h3>
-  </div>
-  <div class="panel-body" style="height: 110px">
-<ul>
-<li>English and foreign language tutoring</li>
-<li>Crontab followup emails</li>
-</ul>
   </div>
 </div>
-</div>
-
-<div class="col-md-6">
-<div class="panel panel-info">
-  <div class="panel-heading">
-    <h3 class="panel-title"><i class="fa fa-wrench"></i> Upcoming Features</h3>
-  </div>
-  <div class="panel-body" style="height: 110px">
-  <ul>
-<li>Database normalization, to more easily add classes</li>
-<li>Improved tutor matching algorithm, see percentage of how well tutors match your search!</li>
-<li>Play around with these new features on our <a href="http://wisner.duckdns.org:8080/index/index">BETA Test Server</a></li>
-</ul>
-  </div>
-</div>
-</div>
-</div>
-<div class="clearfix"></div>
-
-<hr>
-
-<div class="row">
-
-            <div class="col-md-3 col-sm-6 hero-feature">
-                <div class="thumbnail">
-                    <img src="/img/minuteman.jpg" alt="">
-                    </div>
-                    <div class="caption">
-                        <h3>Lexington MA Only</h3>
-                        <p>This site was designed exclusively for students, parents, and tutors of the Lexington Community.</p>
-                        <p>
-                            <a href="/about" class="btn btn-default">About Us »</a>
-                        </p>
-                    </div>
-            </div>
-
-            <div class="col-md-3 col-sm-6 hero-feature">
-                <div class="thumbnail">
-                    <img src="/img/search.jpg" alt="">
-                    </div>
-                    <div class="caption">
-                        <h3>Find The Perfect Tutor</h3>
-                        <p>Just select the credentials you require in your tutor, and we will search all our listing to find you the best one. You can even rate tutors. Currently there are  tutors,  of which are active.</p>
-                        <p>
-                            <a href="/search/index" class="btn btn-default">Find a Tutor »</a>
-                        </p>
-                    </div>
-
-            </div>
-
-            <div class="col-md-3 col-sm-6 hero-feature">
-                <div class="thumbnail">
-                    <img src="/img/profile.jpg" alt="">
-                    </div>
-                    <div class="caption">
-                        <h3>Become a Tutor Yourself</h3>
-                        <p>Interested in tutoring? We provide a free easy way to be found. Just register as a tutor, and complete your tutor profile. We will do the rest. Both student and professional tutors are welcome.</p>
-                        <p>
-                            <a href="/auth/register" class="btn btn-default">Start Tutoring »</a>
-                        </p>
-                    </div>
-
-            </div>
-
-            <div class="col-md-3 col-sm-6 hero-feature">
-                <div class="thumbnail">
-                    <img src="/img/github.jpg" alt="">
-                    </div>
-                    <div class="caption">
-                        <h3>Site Run & Created by LHS Students</h3>
-                        <p>This entire site is hand-coded, designed, and run by Lexington High School seniors.</p>
-                        <p>
-                            <a href="https://github.com/wizard97/tutor-exchange" class="btn btn-default">Source Code »</a>
-                        </p>
-                    </div>
-
-            </div>
-
-        </div>
-  </div>
+<script>
+$(document).ready(function() {
+  $('.carousel').carousel({
+    interval: 3000
+  })
+})
+</script>
 @stop
