ff506de14bea2d7429811b48d1343d8b5adc0795
Aaron Wisner <aaronwisner@gmail.com>
2015-09-06 06:04:50 +0000

    changed button class colors for new theme

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -7,7 +7,7 @@
   </div>
   <div class="col-md-6 col-md-offset-3 ">
     <div class="well">
-      <a class="btn btn-primary btn-block btn-lg" href="{{ route('school.index') }}"><i class="fa fa-university"></i> School Tutoring</a>
+      <a class="btn btn-info btn-block btn-lg" href="{{ route('school.index') }}"><i class="fa fa-university"></i> School Tutoring</a>
       <a class="btn btn-success btn-block btn-lg" href="{{ route('music.index') }}"><i class="fa fa-music"></i> Music Tutoring</a>
       <a class="btn btn-warning btn-block btn-lg disabled" href=""><i class="fa fa-question-circle"></i> Other <span class="label label-info">Under development...</span></a>
     </div>
