7626d785c35a03a447f8570b168a40c1f2dc9740
Aaron Wisner <aaronwisner@gmail.com>
2015-09-23 04:27:55 +0000

    Feedback Messages

--- resources/views/templates/feedback.blade.php
+++ resources/views/templates/feedback.blade.php
@@ -1,4 +1,13 @@
+<?php
+//pull out all the session stuff first
+$fp = \Session::pull('feedback_positive');
+$fw = \Session::pull('feedback_warning');
+$fn = \Session::pull('feedback_negative');
 
+\Request::session()->forget('feedback_positive');
+\Request::session()->forget('feedback_warning');
+\Request::session()->forget('feedback_negative');
+?>
 <div id="feedback">
 @if (count($errors) > 0)
 <!-- Also can be printed out with javascript-->
@@ -13,27 +22,24 @@
 </div>
 @endif
 
-@if(Session::has('feedback_positive'))
+@if($fp)
 <div class="alert alert-success alert-dismissible" role="alert">
   <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-  <h4><i class="fa fa-thumbs-up"></i> Nice Work! </h4>{!! Session::pull('feedback_positive') !!}</div>
+  <h4><i class="fa fa-thumbs-up"></i> Nice Work! </h4>{!! $fp !!}</div>
 @endif
 
-@if(Session::has('feedback_warning'))
+@if($fw)
 <div class="alert alert-warning alert-dismissible" role="alert">
   <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-  <h4><i class="fa fa-info-circle"></i> Watch out! </h4>{!! Session::pull('feedback_warning') !!}</div>
+  <h4><i class="fa fa-info-circle"></i> Watch out! </h4>{!! $fw !!}</div>
 @endif
 
-@if(Session::has('feedback_negative'))
+@if($fn)
 <div class="alert alert-danger alert-dismissible" role="alert">
   <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-  <h4><i class="fa fa-info-circle"></i> Watch out! </h4>{!! Session::pull('feedback_negative') !!}</div>
+  <h4><i class="fa fa-info-circle"></i> Watch out! </h4>{!! $fn !!}</div>
 @endif
 </div>
-
 <?php
-\Request::session()->forget('feedback_positive');
-\Request::session()->forget('feedback_negative');
 unset($errors);
-?>
+ ?>
