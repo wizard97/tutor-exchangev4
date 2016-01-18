8239b8cb1d73363a43d6948248e9fed96a9c5b77
MatanSilver <matansilver@gmail.com>
2016-01-18 09:45:34 -0500

    fixed instantiations for typeahead text fields

--- resources/views/account/tutoring/submitschool.blade.php
+++ resources/views/account/tutoring/submitschool.blade.php
@@ -46,7 +46,45 @@
                   <div class="col-xs-9">
 
                     <!--<input type="text" class="typeahead form-control" id="searchform" spellcheck="false">-->
-                    <input type="text" class="typeahead form-control" id="searchform" data-provide="typeahead">
+                    <input type="text" class="typeahead form-control" id="submitschoolname" data-provide="typeahead">
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-xs-3">
+                    School Address: <span class="text text-danger">*</span>
+                  </div>
+                  <div class="col-xs-9">
+
+                    <input type="text" class="typeahead form-control" spellcheck="false">
+
+                  </div>
+                </div>
+                <hr>
+                <span class="text text-danger">* = Required</span>
+
+                <button class="btn btn-success pull-right" type="button">
+                  Submit
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div role="tabpanel" class="tab-pane fade in" id="edit">
+          <div class="col-md-8">
+            <div class="panel panel-default">
+              <div class="panel-heading"> <!-- <i class="fa fa-bars"></i> School Classes -->
+                Enter School Info
+              </div>
+              <div class="panel-body">
+                <div class="row">
+                  <div class="col-xs-3">
+                    School Name: <span class="text text-danger">*</span>
+                  </div>
+                  <div class="col-xs-9">
+
+                    <!--<input type="text" class="typeahead form-control" id="searchform" spellcheck="false">-->
+                    <input type="text" class="typeahead form-control" id="editschoolname" data-provide="typeahead">
                   </div>
                 </div>
                 <div class="row">
@@ -72,19 +110,26 @@
         <div role="tabpanel" class="tab-pane fade in" id="delete">
           <div class="col-md-8">
             <div class="panel panel-default">
-              <div class="panel-heading">
+              <div class="panel-heading"> <!-- <i class="fa fa-bars"></i> School Classes -->
+                Enter School Info
               </div>
               <div class="panel-body">
-              </div>
-            </div>
-          </div>
-        </div>
-        <div role="tabpanel" class="tab-pane fade in" id="edit">
-          <div class="col-md-8">
-            <div class="panel panel-default">
-              <div class="panel-heading">
-              </div>
-              <div class="panel-body">
+                <div class="row">
+                  <div class="col-xs-3">
+                    School Name: <span class="text text-danger">*</span>
+                  </div>
+                  <div class="col-xs-9">
+
+                    <!--<input type="text" class="typeahead form-control" id="searchform" spellcheck="false">-->
+                    <input type="text" class="typeahead form-control" id="deleteschoolname" data-provide="typeahead">
+                  </div>
+                </div>
+                <hr>
+                <span class="text text-danger">* = Required</span>
+
+                <button class="btn btn-danger pull-right" type="button">
+                  Delete
+                </button>
               </div>
             </div>
           </div>
@@ -95,17 +140,87 @@
 </div>
 
 <script>
-$(document).ready(function(){
-
-  $('#searchform').typeahead(null,
-    {
-      name: 'planets',
-      local: [ "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune" ]
-    });
-
+// instantiate the bloodhound suggestion engine
+$( document ).ready(function() {
+  var schools = new Bloodhound({
+    sufficient: 10,
+    identify: function(obj) { return obj.id; },
+    queryTokenizer: function(query) {
+      var no_commas = query.replace(/,/g , '');
+      return Bloodhound.tokenizers.whitespace(no_commas);
+    },
+    datumTokenizer: function(datum) {
+      var tokens = [];
+      tokens.push(String(datum.school_name));
+      tokens.push(String(datum.city));
+      tokens.push(String(datum.zip_code));
+      tokens.push(String(datum.state_prefix));
+      tokens.push(String(datum.school_id));
+      return tokens;
+    },
+    prefetch: "{{route('hs.prefetch')}}",
+    remote: {
+      url: '{{ route('hs.query', ['query' => '%QUERY']) }}',
+      wildcard: '%QUERY'
+    },
   });
 
 
-});
-</script>
+  $('#editschoolname').typeahead(null, //instantiate edit typeahead
+    {
+      source: schools.ttAdapter(),
+      display: 'response',
+      limit: 5,
+      templates: {
+        notFound: [
+          '<p class="empty-message tt-suggestion">',
+          '<strong>Sorry, no tutors belong to that school.</strong>',
+          '</p>'
+        ].join('\n'),
+        suggestion: function(data) {
+          return '<p><strong>' + data.school_name + ',</strong> <small>' + data.city + ', '+ data.state_prefix + ' '+ data.zip_code + '</small></p>';
+        }
+
+      }
+    });
+
+    $('#submitschoolname').typeahead(null, //instantiate submit typeahead
+      {
+        source: schools.ttAdapter(),
+        display: 'response',
+        limit: 5,
+        templates: {
+          notFound: [
+            '<p class="empty-message tt-suggestion">',
+            '<strong>Sorry, no tutors belong to that school.</strong>',
+            '</p>'
+          ].join('\n'),
+          suggestion: function(data) {
+            return '<p><strong>' + data.school_name + ',</strong> <small>' + data.city + ', '+ data.state_prefix + ' '+ data.zip_code + '</small></p>';
+          }
+
+        }
+      });
+
+      $('#deleteschoolname').typeahead(null, //instantiate submit typeahead
+        {
+          source: schools.ttAdapter(),
+          display: 'response',
+          limit: 5,
+          templates: {
+            notFound: [
+              '<p class="empty-message tt-suggestion">',
+              '<strong>Sorry, no tutors belong to that school.</strong>',
+              '</p>'
+            ].join('\n'),
+            suggestion: function(data) {
+              return '<p><strong>' + data.school_name + ',</strong> <small>' + data.city + ', '+ data.state_prefix + ' '+ data.zip_code + '</small></p>';
+            }
+
+          }
+        });
+
+  });
+
+  </script>
 @stop
