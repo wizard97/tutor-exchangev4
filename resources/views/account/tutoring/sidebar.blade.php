3201a4596be839ca495c6510650e0602f8e13970
MatanSilver <matansilver@gmail.com>
2016-01-20 18:36:41 -0500

    added links on desktop and mobile for submitting subjects

--- resources/views/account/tutoring/sidebar.blade.php
+++ resources/views/account/tutoring/sidebar.blade.php
@@ -8,6 +8,7 @@
     <li class="{{ isActiveRoute('tutoring.music') }}"><a href="{{route('tutoring.music')}}"><i class="fa fa-music fa-fw"></i> My Music</a></li>
     <li class="{{ isActiveRoute('tutoring.myprofile') }}"><a href="{{route('tutoring.myprofile')}}"><i class="fa fa-user fa-fw"></i> View My Profile</a></li>
     <li class="{{ isActiveRoute('tutoring.submitschool') }}"><a href="{{route('tutoring.submitschool')}}"><i class="fa fa-book fa-fw"></i> Submit/Edit School</a></li>
+    <li class="{{ isActiveRoute('tutoring.submitsubject') }}"><a href="{{route('tutoring.submitsubject')}}"><i class="fa fa-book fa-fw"></i> Submit/Edit Subject</a></li>
     <li class="{{ isActiveRoute('tutoring.submitclass') }}"><a href="{{route('tutoring.submitclass')}}"><i class="fa fa-book fa-fw"></i> Submit/Edit Class/Level</a></li>
 
   </ul>
@@ -56,6 +57,10 @@
             <a class="btn btn-primary" href="{{ route('tutoring.submitschool') }}">
               <i class="fa fa-user fa-2x"></i><br>
               Submit/Edit School
+            </a>
+            <a class="btn btn-primary" href="{{ route('tutoring.submitsubject') }}">
+              <i class="fa fa-user fa-2x"></i><br>
+              Submit/Edit Subject
             </a>
             <a class="btn btn-primary" href="{{ route('tutoring.submitclass') }}">
               <i class="fa fa-user fa-2x"></i><br>
