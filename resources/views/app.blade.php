6403bc05aab12bc9e303f4d24b46a530d5cdd8a9
MatanSilver <matansilver@gmail.com>
2016-02-25 19:54:04 -0500

    removed title saying only for lex

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="csrf-token" content="{{ csrf_token() }}">
-      <title>Lexington Tutor Exchange, Exclusively For Lexington MA Tutoring</title>
+      <title>Lexington Tutor Exchange</title>
       <meta name="description" content="">
       <meta name="viewport" content="width=device-width, initial-scale=1">
 
