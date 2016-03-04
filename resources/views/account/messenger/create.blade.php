787a6e874304798f456962d24bdd63c7d59056a9
MatanSilver <matansilver@gmail.com>
2016-03-04 22:39:21 +0000

    fixed sending messages

--- resources/views/account/messenger/create.blade.php
+++ resources/views/account/messenger/create.blade.php
@@ -1,45 +1,29 @@
 @extends('app')
-
 @section('content')
 <div class="container">
   <h1>Create a new message</h1>
   {!! Form::open(['route' => 'messages.store']) !!}
   <div class="col-md-6">
-      <!-- Subject Form Input -->
       <div class="form-group">
         {!! Form::label('recipients', 'Recipients', ['class' => 'control-label']) !!}
         {!! Form::text('recipients', null, ['class' => 'form-control typeahead', 'id' => 'recipients', 'data-provide' => 'typeahead', 'autocomplete' => 'off', 'placeholder' => 'e.g. John Smith']) !!}
-
       </div>
       <div class="form-group">
           {!! Form::label('subject', 'Subject', ['class' => 'control-label']) !!}
           {!! Form::text('subject', null, ['class' => 'form-control']) !!}
       </div>
-
-      <!-- Message Form Input -->
       <div class="form-group">
           {!! Form::label('message', 'Message', ['class' => 'control-label']) !!}
           {!! Form::textarea('message', null, ['class' => 'form-control']) !!}
       </div>
   </div>
   <div class="col-md-12">
-
-    <!--  @if($users->count() > 0)
-      <div class="checkbox">
-          @foreach($users as $user)
-              <label title="{!!$user->name!!}"><input type="checkbox" name="recipients[]" value="{!!$user->id!!}">{!!$user->fname!!}</label>
-          @endforeach
-      </div>
-      @endif-->
-
-      <!-- Submit Form Input -->
       <div class="form-group">
           {!! Form::submit('Submit', ['class' => 'btn btn-primary form-control']) !!}
       </div>
   </div>
   {!! Form::close() !!}
 </div>
-
 <script>
 $(document).ready(function() {
   var recipientshound = new Bloodhound({
@@ -66,11 +50,13 @@ $(document).ready(function() {
     typeaheadjs: {
       name: 'name',
       displayKey: 'fname',
-      valueKey: 'fname',
-      source: recipientshound.ttAdapter()
+      valueKey: 'id',
+      source: recipientshound.ttAdapter(),
+      suggestion: function (user) {
+            return '<p>' + user.fname + '</p>';
+        }
     }
   });
 });
-
 </script>
 @stop
