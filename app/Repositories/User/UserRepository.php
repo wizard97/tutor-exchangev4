6b40df9d3e99f3358396f22a0ce7410faf1c37af
MatanSilver <matansilver@gmail.com>
2016-03-05 02:10:02 +0000

    concatenated first and last names in message recipients

--- app/Repositories/User/UserRepository.php
+++ app/Repositories/User/UserRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories\User;
 use App\Repositories\BaseRepository;
 
 use App\Models\User\User;
+use DB;
 
 class UserRepository extends BaseRepository implements UserRepositoryContract
 {
@@ -21,7 +22,9 @@ class UserRepository extends BaseRepository implements UserRepositoryContract
         $query->orWhere('fname', 'LIKE', '%'.$key.'%')
         ->orWhere('lname', 'LIKE', '%'.$key.'%');
       }
-    })->select('fname', 'lname', 'id')
+    })
+    //->select('fname', 'lname', 'id')
+    ->select(DB::raw("CONCAT(users.fname,' ',users.lname) AS full_name"), 'id')
     ->take(10)
     ->get();
     return $search;
@@ -29,7 +32,8 @@ class UserRepository extends BaseRepository implements UserRepositoryContract
   public function possibleRecipientsPrefetch()
   {
     $prefetch = User::all()
-    ->select('fname', 'lname', 'id')
+    //->select('fname', 'lname', 'id')
+    ->select(DB::raw("CONCAT(users.fname,' ',users.lname) AS full_name"), 'id')
     ->take(100);
     return $prefetch;
   }
