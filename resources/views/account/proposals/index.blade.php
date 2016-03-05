e233c50cf635d187cb80f8f86f03fa1bd00a6428
MatanSilver <matansilver@gmail.com>
2016-03-05 13:57:41 -0500

    Added links for contributions

--- resources/views/account/proposals/index.blade.php
+++ resources/views/account/proposals/index.blade.php
@@ -2,7 +2,10 @@
 @extends('app')
 @section('content')
 <div class="container">
-{{ $user->getName() }}
+{{ \Auth::user()->getName() }}
+<a href="{{ route('proposals.submitclass') }}"><button class="btn btn-primary">Submit Class</button></a>
+<a href="{{ route('proposals.submitschool') }}"><button class="btn btn-primary">Submit School</button></a>
+<a href="{{ route('proposals.submitsubject') }}"><button class="btn btn-primary">Submit Subject</button></a>
 </div>
 <script>
 </script>
