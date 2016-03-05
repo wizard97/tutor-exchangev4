d507891250277f60845c28c881d625457364e0ae
MatanSilver <matansilver@gmail.com>
2016-03-05 12:18:58 -0500

    attempt to fix id/value discrepancy

--- resources/views/account/messenger/create.blade.php
+++ resources/views/account/messenger/create.blade.php
@@ -4,23 +4,23 @@
   <h1>Create a new message</h1>
   {!! Form::open(['route' => 'messages.store']) !!}
   <div class="col-md-6">
-      <div class="form-group">
-        {!! Form::label('recipients', 'Recipients', ['class' => 'control-label']) !!}
-        {!! Form::text('recipients', null, ['class' => 'form-control typeahead', 'id' => 'recipients', 'data-provide' => 'typeahead', 'autocomplete' => 'off', 'placeholder' => 'e.g. John Smith']) !!}
-      </div>
-      <div class="form-group">
-          {!! Form::label('subject', 'Subject', ['class' => 'control-label']) !!}
-          {!! Form::text('subject', null, ['class' => 'form-control']) !!}
-      </div>
-      <div class="form-group">
-          {!! Form::label('message', 'Message', ['class' => 'control-label']) !!}
-          {!! Form::textarea('message', null, ['class' => 'form-control']) !!}
-      </div>
+    <div class="form-group">
+      {!! Form::label('recipients', 'Recipients', ['class' => 'control-label']) !!}
+      {!! Form::text('recipients', null, ['class' => 'form-control typeahead', 'id' => 'recipients', 'data-provide' => 'typeahead', 'autocomplete' => 'off', 'placeholder' => 'e.g. John Smith']) !!}
+    </div>
+    <div class="form-group">
+      {!! Form::label('subject', 'Subject', ['class' => 'control-label']) !!}
+      {!! Form::text('subject', null, ['class' => 'form-control']) !!}
+    </div>
+    <div class="form-group">
+      {!! Form::label('message', 'Message', ['class' => 'control-label']) !!}
+      {!! Form::textarea('message', null, ['class' => 'form-control']) !!}
+    </div>
   </div>
   <div class="col-md-12">
-      <div class="form-group">
-          {!! Form::submit('Submit', ['class' => 'btn btn-primary form-control']) !!}
-      </div>
+    <div class="form-group">
+      {!! Form::submit('Submit', ['class' => 'btn btn-primary form-control']) !!}
+    </div>
   </div>
   {!! Form::close() !!}
 </div>
@@ -28,14 +28,15 @@
 $(document).ready(function() {
   var recipientshound = new Bloodhound({
     sufficient: 10,
-    identify: function(obj) { return obj.title; },
+    identify: function(obj) { return obj.id; },
     queryTokenizer: function(query) {
       var no_commas = query.replace(/,/g, '');
-      return Bloodhound.tokenizers.whitespace(no_commas);
+      return Bloodhound.tokenizers.obj.whitespace(no_commas);
     },
     datumTokenizer: function(datum) {
       var tokens = [];
-      tokens.push(String(datum.title));
+      tokens.push(String(datum.full_name));
+      tokens.push(datum.id);
       return tokens;
     },
     prefetch: "{{ route('messages.prefetch') }}",
@@ -45,16 +46,31 @@ $(document).ready(function() {
     },
   });
   $('#recipients').tagsinput({
-    //itemValue: 'id',
-    //itemText: 'fname',
+    itemValue: 'id',
+    itemText: 'full_name',
+    tagClass: function(user) {
+      switch (user.account_type) {
+        case 1  : return 'label label-primary';
+        case 2  : return 'label label-warning';
+        case 3  : return 'label label-success';
+      }
+    },
     typeaheadjs: {
       name: 'name',
       displayKey: 'full_name',
-      valueKey: 'id',
+      //display: 'users',
+      //valueKey: 'id',
       source: recipientshound.ttAdapter(),
-      suggestion: function (user) {
-            return '<p>' + user.full_name + '</p>';
+      templates: {
+        notFound: [
+          '<p class="empty-message tt-suggestion">',
+          '<strong>No users with that name.</strong>',
+          '</p>'
+        ].join('\n'),
+        suggestion: function (user) {
+          return '<p>' + user.full_name + '</p>' + '<p class="text-uppercase text-muted">' + user.address + '</p>';
         }
+      }
     }
   });
 });
