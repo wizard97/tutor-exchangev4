90b240e5a9175af282894d4e19b21d3e3868fb48
MatanSilver <matansilver@gmail.com>
2016-03-11 01:21:20 +0000

    fixed html tag mismatch

--- resources/views/search/school/schools.blade.php
+++ resources/views/search/school/schools.blade.php
@@ -1,79 +1,71 @@
 @extends('app')
-
 @section('content')
-
 <div class="container">
-
-  <div class="page-header">
-    <h1>Select Your School</h1>
-  </div>
-  @include('templates/feedback')
-  <div class="col-md-6 col-md-offset-3">
-    <div class="well">
-    <form action="{{ route('hs.submitschool') }}" method="POST">
-      {!! csrf_field() !!}
-
-      <div class="form-group">
-        <label for="school-input">School Name</label>
-        <div class="input-group">
-          <input type="search" class="typeahead form-control" id="school-input" name="school_name">
-          <span class="input-group-btn">
-          <button class="btn btn-success" type="submit">Submit</button>
-        </span>
+    <div class="page-header">
+        <h1>Select Your School</h1>
+    </div>
+    @include('templates/feedback')
+    <div class="col-md-6 col-md-offset-3">
+        <div class="well">
+            <form action="{{ route('hs.submitschool') }}" method="POST">
+                {!! csrf_field() !!}
+                <div class="form-group">
+                    <label for="school-input">School Name</label>
+                    <div class="input-group">
+                        <input type="search" class="typeahead form-control" id="school-input" name="school_name">
+                        <span class="input-group-btn">
+                            <button class="btn btn-success" type="submit">Submit</button>
+                        </span>
+                    </div>
+                </div>
+            </form>
         </div>
-      </div>
     </div>
-    </div>
-    </form>
-  </div>
-
-  <script>
-  // instantiate the bloodhound suggestion engine
-  $( document ).ready(function() {
+</div>
+<script>
+// instantiate the bloodhound suggestion engine
+$( document ).ready(function() {
     var schools = new Bloodhound({
-      sufficient: 10,
-      identify: function(obj) { return obj.id; },
-      queryTokenizer: function(query) {
-        var no_commas = query.replace(/,/g , '');
-        return Bloodhound.tokenizers.whitespace(no_commas);
-      },
-      datumTokenizer: function(datum) {
-        var tokens = [];
-        tokens.push(String(datum.school_name));
-        tokens.push(String(datum.city));
-        tokens.push(String(datum.zip_code));
-        tokens.push(String(datum.state_prefix));
-        tokens.push(String(datum.school_id));
-        return tokens;
-      },
-      prefetch: "{{route('hs.prefetch')}}",
-      remote: {
-        url: '{{ route('hs.query', ['query' => '%QUERY']) }}',
-        wildcard: '%QUERY'
-      },
+        sufficient: 10,
+        identify: function(obj) { return obj.id; },
+        queryTokenizer: function(query) {
+            var no_commas = query.replace(/,/g , '');
+            return Bloodhound.tokenizers.whitespace(no_commas);
+        },
+        datumTokenizer: function(datum) {
+            var tokens = [];
+            tokens.push(String(datum.school_name));
+            tokens.push(String(datum.city));
+            tokens.push(String(datum.zip_code));
+            tokens.push(String(datum.state_prefix));
+            tokens.push(String(datum.school_id));
+            return tokens;
+        },
+        prefetch: "{{route('hs.prefetch')}}",
+        remote: {
+            url: '{{ route('hs.query', ['query' => '%QUERY']) }}',
+            wildcard: '%QUERY'
+        },
     });
-
-
     $('.typeahead').typeahead(null,
-      {
-        source: schools.ttAdapter(),
-        display: 'response',
-        limit: 5,
-        templates: {
-          notFound: [
-            '<p class="empty-message tt-suggestion">',
-            '<strong>Sorry, no tutors belong to that school.</strong>',
-            '</p>'
-          ].join('\n'),
-          suggestion: function(data) {
-            return '<p><strong>' + data.school_name + ',</strong> <small>' + data.city + ', '+ data.state_prefix + ' '+ data.zip_code + '</small></p>';
-          }
+        {
+            source: schools.ttAdapter(),
+            display: 'response',
+            limit: 5,
+            templates: {
+                notFound: [
+                    '<p class="empty-message tt-suggestion">',
+                    '<strong>Sorry, no tutors belong to that school.</strong>',
+                    '</p>'
+                ].join('\n'),
+                suggestion: function(data) {
+                    return '<p><strong>' + data.school_name + ',</strong> <small>' + data.city + ', '+ data.state_prefix + ' '+ data.zip_code + '</small></p>';
+                }
 
-        }
-      });
+            }
+        });
 
     });
-
     </script>
-  </div>
-  @stop
+</div>
+@stop
